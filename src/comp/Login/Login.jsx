2867b35b0a236faf7dbfc5f5379977546921f289
Abdalrahman15 <abdalrahman200324@gmail.com>
2025-03-07 14:49:53 +0200

    up

--- src/comp/Login/Login.jsx
+++ src/comp/Login/Login.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios'
 import { useFormik } from 'formik'
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import SunAndMoonProvider, { SunAndMoon } from '../../Context/SunAndMoon.jsx'
+import { SunAndMoon } from '../../Context/SunAndMoon.jsx'
 
 export default function Login() {
     const [sucess, setSucess] = useState(null)
@@ -11,101 +11,63 @@ export default function Login() {
     const [toggle, setToggle] = useState(false)
     const { togglex } = useContext(SunAndMoon)
     let nav = useNavigate()
-    
-    
 
-
-    async function handleLogin (values){
-        try{
-            let res=await  axios.post("https://ecommerce.routemisr.com/api/v1/auth/signin",values)
+    async function handleLogin(values) {
+        try {
+            let res = await axios.post("https://ecommerce.routemisr.com/api/v1/auth/signin", values)
             console.log(res)
-       
-                setSucess(res?.data?.message)
-                setToken(res.data.token)
-                localStorage.setItem("token",res.data.token)
-                nav("/home")
-                setFaild(" ")
-            
-        }catch(err){
+
+            setSucess(res?.data?.message)
+            setToken(res.data.token)
+            localStorage.setItem("token", res.data.token)
+            nav("/home")
+            setFaild(" ")
+
+        } catch (err) {
             console.log(err)
             setFaild(err.response.data.message)
             setSucess(" ")
         }
-       
-        
     }
 
-   
-
-  let formik = useFormik({
-    initialValues:{
-        
-            
-            email:"",
-            password:"",
-        
-    },onSubmit:handleLogin
-    
-  })
-
-
-  console.log(token,"uuuuuuuuu")
-
-
-
- 
-
-
-
-
-
-
-
+    let formik = useFormik({
+        initialValues: {
+            email: "",
+            password: "",
+        },
+        onSubmit: handleLogin
+    })
 
     return <>
+        <form className="max-w-sm mx-auto mt-[100px]" onSubmit={formik.handleSubmit}>
+            <div className="mb-5">
+                <label htmlFor="email" className={`block mb-2 text-sm font-medium ${togglex == false ? "text-white" : "text-gray-950"}`}>Your email</label>
+                <input onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.email} type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="" required />
+            </div>
 
+            <div className="mb-5 relative">
+                <label htmlFor="password" className={`block mb-2 text-sm font-medium ${togglex == false ? "text-white" : "text-gray-950"}`}>
+                    Your password
+                </label>
+                <input
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.password}
+                    type={toggle ? "text" : "password"}
+                    id="password"
+                    className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ${togglex == false ? "text-gray-950" : "text-red-800"}`}
+                    placeholder=""
+                    required
+                />
+                <i
+                    onClick={() => setToggle(!toggle)}
+                    className={`absolute top-1/2 right-4 -translate-y-1/2 cursor-pointer mt-3 ${toggle ? "text-yellow-600 fa-solid fa-eye" : "fa-solid fa-eye-slash"}`}
+                ></i>
+            </div>
 
-<form className="max-w-sm mx-auto mt-[100px]" onSubmit={formik.handleSubmit}>
- 
-  
-  <div className="mb-5">
-    <label htmlFor="email"  className={`block mb-2 text-sm font-medium  ${togglex==false?"text-white":"text-gray-950"}`}>Your email</label>
-    <input onChange={formik.handleChange} onBlur={formik.handleBlur}  value={formik.values.email} type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required />
-  </div>
-
-  <div className="mb-5 relative">
-  <label htmlFor="password" className={`block mb-2 text-sm font-medium  ${togglex==false?"text-white":"text-gray-950"}`}>
-    Your password
-  </label>
-  <input
-    onChange={formik.handleChange}
-    onBlur={formik.handleBlur}
-    value={formik.values.password}
-    type={toggle ? "text" : "password"}
-    id="password"
-    className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 ${togglex==false?"text-gray-950":"text-red-800"}`}
-    placeholder=""
-    required
-  />
-  <i
-    onClick={() => setToggle(!toggle)}
-    className={`absolute top-1/2 right-4 -translate-y-1/2 cursor-pointer mt-3 ${
-      toggle ? "text-yellow-600 fa-solid fa-eye" : "fa-solid fa-eye-slash"
-    }`}
-  ></i>
-</div>
-
-
-
-
-  
-  <button type="submit" className="text-white bg-black hover:bg-yellow-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-  <h1>{sucess}</h1>
-  <h1>{faild}</h1>
-</form>
-
-
-    
-  </>
-  
+            <button type="submit" className="text-white bg-black hover:bg-yellow-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Submit</button>
+            <h1>{sucess}</h1>
+            <h1>{faild}</h1>
+        </form>
+    </>
 }
--- src/comp/Navbar/Navbar.jsx
+++ src/comp/Navbar/Navbar.jsx
@@ -34,14 +34,14 @@ export default function Navbar() {
   return <>
 
 
-<nav className=" border-gray-200 dark:bg-gray-900 dark:border-gray-700 fixed w-full bg-black z-[999] ">
+<nav className=" border-gray-200  fixed w-full bg-black z-[999] ">
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
     <div to="#" className="flex flex-wrap items-center space-x-3 rtl:space-x-reverse  lg:w-1/3 md:w-auto ">
     <i class="fa-solid fa-dumbbell text-white text-3xl"></i>
-      <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white text-white">Project Test</span>
+      <span className="self-center text-2xl font-semibold whitespace-nowrap  text-white">Project Test</span>
     </div>
     
-    <button onClick={()=>setToggle(!toggle)} data-collapse-toggle="navbar-multi-level" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg md:hidden hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 z-[999999999999]" aria-controls="navbar-multi-level" aria-expanded="false">
+    <button onClick={()=>setToggle(!toggle)} data-collapse-toggle="navbar-multi-level" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg md:hidden hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-gray-200  z-[999999999999]" aria-controls="navbar-multi-level" aria-expanded="false">
       <span className="sr-only">Open main menu</span>
       <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M1 1h15M1 7h15M1 13h15" />
@@ -49,83 +49,83 @@ export default function Navbar() {
     </button>
 
     <div className={toggle==true? "hidden  lg:w-1/2 md:w-auto md:block   " :"  w-full md:block md:w-auto" }id="navbar-multi-level">
-      <ul className="lg:gap-10 md:gap-7  flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg  md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 bg-black">
+      <ul className="lg:gap-10 md:gap-7  flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg  md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0  bg-black">
        
-        <div className='flex flex-col font-medium p-4 md:p-0    md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 bg-black'>
+        <div className='flex flex-col font-medium p-4 md:p-0    md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0  bg-black'>
         <li className='bg-black'>
-          <NavLink to="home" className=" block py-2 px-3 text-white rounded-sm aria-[current=page]:bg-yellow-600 md:p-0 md:dark:text-blue-500 md:dark:bg-transparent dark:bg-blue-600 md:aria-[current=page]:bg-transparent md:aria-[current=page]:text-yellow-600 " onClick={()=>setToggle(true)}>Home</NavLink>
+          <NavLink to="home" className=" block py-2 px-3 text-white rounded-sm aria-[current=page]:bg-yellow-600 md:p-0  md:aria-[current=page]:bg-transparent md:aria-[current=page]:text-yellow-600 " onClick={()=>setToggle(true)}>Home</NavLink>
         </li>
         <li className='bg-black'>
-          <NavLink to="products" id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" className="flex items-center justify-between w-full py-2 px-3 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent text-white aria-[current=page]:bg-yellow-600 md:aria-[current=page]:bg-transparent md:aria-[current=page]:text-yellow-600 hover:bg-yellow-600" onClick={()=>setToggle(true)}>Dropdown <svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+          <NavLink to="products" id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" className="flex items-center justify-between w-full py-2 px-3 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto  text-white aria-[current=page]:bg-yellow-600 md:aria-[current=page]:bg-transparent md:aria-[current=page]:text-yellow-600 hover:bg-yellow-600" onClick={()=>setToggle(true)}>Dropdown <svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
               <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="m1 1 4 4 4-4" />
             </svg></NavLink>
           {/* Dropdown menu */}
-          <div id="dropdownNavbar" className=" hidden z-10 font-normal absolute bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700 dark:divide-gray-600">
-            <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownLargeButton">
+          <div id="dropdownNavbar" className=" hidden z-10 font-normal absolute bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 ">
+            <ul className="py-2 text-sm text-gray-700 " aria-labelledby="dropdownLargeButton">
               <li>
-                <NavLink to="products" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</NavLink>
+                <NavLink to="products" className="block px-4 py-2 hover:bg-gray-100 ">Dashboard</NavLink>
               </li>
               <li aria-labelledby="dropdownNavbarLink">
-                <NavLink id="doubleDropdownButton" data-dropdown-toggle="doubleDropdown" data-dropdown-placement="right-start" type="button" className="flex items-center justify-between w-full px-4 py-2 hover:bg-yellow-600 dark:hover:bg-gray-600 dark:hover:text-white text-white">Dropdown<svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                <NavLink id="doubleDropdownButton" data-dropdown-toggle="doubleDropdown" data-dropdown-placement="right-start" type="button" className="flex items-center justify-between w-full px-4 py-2 hover:bg-yellow-600  text-white">Dropdown<svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="m1 1 4 4 4-4" />
                   </svg></NavLink>
-                <div id="doubleDropdown" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700">
-                  <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="doubleDropdownButton">
+                <div id="doubleDropdown" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 ">
+                  <ul className="py-2 text-sm text-gray-700 " aria-labelledby="doubleDropdownButton">
                     <li>
-                      <NavLink to="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white text-white" onClick={()=>setToggle(true)}>Overview</NavLink>
+                      <NavLink to="#" className="block px-4 py-2 hover:bg-gray-100  text-white" onClick={()=>setToggle(true)}>Overview</NavLink>
                     </li>
                     <li>
-                      <NavLink to="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white" onClick={()=>setToggle(true)}>My downloads</NavLink>
+                      <NavLink to="#" className="block px-4 py-2 hover:bg-gray-100 " onClick={()=>setToggle(true)}>My downloads</NavLink>
                     </li>
                     <li>
-                      <NavLink to="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white" onClick={()=>setToggle(true)}>Billing</NavLink>
+                      <NavLink to="#" className="block px-4 py-2 hover:bg-gray-100 " onClick={()=>setToggle(true)}>Billing</NavLink>
                     </li>
                     <li>
-                      <NavLink to="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white" onClick={()=>setToggle(true)}>Rewards</NavLink>
+                      <NavLink to="#" className="block px-4 py-2 hover:bg-gray-100 " onClick={()=>setToggle(true)}>Rewards</NavLink>
                     </li>
                   </ul>
                 </div>
               </li>
               <li>
-                <NavLink to="#" className="block px-4 py-2 hover:bg-yellow-600 dark:hover:bg-gray-600 dark:hover:text-white text-white" onClick={()=>setToggle(true)}>Earnings</NavLink>
+                <NavLink to="#" className="block px-4 py-2 hover:bg-yellow-600  text-white" onClick={()=>setToggle(true)}>Earnings</NavLink>
               </li>
             </ul>
             <div className="py-1">
-              <NavLink to="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white" onClick={()=>setToggle(true)}>Sign out</NavLink>
+              <NavLink to="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 " onClick={()=>setToggle(true)}>Sign out</NavLink>
             </div>
           </div>
         </li>
         <li>
-          <NavLink to="#" className="block py-2 px-3  rounded-sm hover:bg-yellow-600 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent text-white" onClick={()=>setToggle(true)}>Services</NavLink>
+          <NavLink to="#" className="block py-2 px-3  rounded-sm hover:bg-yellow-600 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 text-white" onClick={()=>setToggle(true)}>Services</NavLink>
         </li>
         <li>
-          <NavLink to="#" className="block py-2 px-3  rounded-sm hover:bg-yellow-600 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent text-white" onClick={()=>setToggle(true)}>Pricing</NavLink>
+          <NavLink to="#" className="block py-2 px-3  rounded-sm hover:bg-yellow-600 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 text-white" onClick={()=>setToggle(true)}>Pricing</NavLink>
         </li>
         <li className='bg-black'>
-          <NavLink to="#" className="block py-2 px-3  rounded-sm hover:bg-yellow-600 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent text-white" onClick={()=>setToggle(true)}>Contact</NavLink>
+          <NavLink to="#" className="block py-2 px-3  rounded-sm hover:bg-yellow-600 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 text-white" onClick={()=>setToggle(true)}>Contact</NavLink>
         </li>
 
         </div>
 
-        <div className='flex flex-col font-medium p-4 md:p-0    md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 bg-black'>
+        <div className='flex flex-col font-medium p-4 md:p-0    md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0   bg-black'>
         <li className='bg-black'>
-          <button onClick={()=>setDropDwon(!dropdown)} id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" className="flex items-center justify-between w-full py-2 px-3 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent text-white hover:bg-yellow-600 ">
+          <button onClick={()=>setDropDwon(!dropdown)} id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" className="flex items-center justify-between w-full py-2 px-3 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto  text-white hover:bg-yellow-600 ">
             My account 
             <svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
               <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="m1 1 4 4 4-4" />
             </svg></button>
           {/* Dropdown menu */}
-          <div id="dropdownNavbar" className={dropdown==true?"hidden z-10 font-normal absolute bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700 dark:divide-gray-600":" z-10 font-normal absolute bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700 dark:divide-gray-600"}>
-            <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownLargeButton">
+          <div id="dropdownNavbar" className={dropdown==true?"hidden z-10 font-normal absolute bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 ":" z-10 font-normal absolute bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44"}>
+            <ul className="py-2 text-sm text-gray-700 " aria-labelledby="dropdownLargeButton">
               <li>
-                <NavLink to="signup" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" onClick={()=>setToggle(true)}>Sign up</NavLink>
+                <NavLink to="signup" className="block px-4 py-2 hover:bg-gray-100 " onClick={()=>setToggle(true)}>Sign up</NavLink>
               </li>
               <li>
-                <NavLink to="login" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white " onClick={()=>setToggle(true)}>Logn in</NavLink>
+                <NavLink to="login" className="block px-4 py-2 hover:bg-gray-100  " onClick={()=>setToggle(true)}>Logn in</NavLink>
               </li>
             </ul>
             <div className="py-1">
-              <NavLink to="" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white" onClick={()=>setToggle(true)}>Sign out</NavLink>
+              <NavLink to="" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 " onClick={()=>setToggle(true)}>Sign out</NavLink>
             </div>
           </div>
         </li>
